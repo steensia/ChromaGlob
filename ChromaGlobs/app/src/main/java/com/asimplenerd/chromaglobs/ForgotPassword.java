22cf07f3397c4b9aeaf80483ec7bea35e343244b
Steen Sia <steensia101@gmail.com>
2020-03-21 21:44:35 -0600

    Created ContactFragment and Finished its Design Prototype

--- ChromaGlobs/app/src/main/java/com/asimplenerd/chromaglobs/ForgotPassword.java
+++ ChromaGlobs/app/src/main/java/com/asimplenerd/chromaglobs/ForgotPassword.java
@@ -1,8 +1,6 @@
 package com.asimplenerd.chromaglobs;
 
-import android.content.Context;
 import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -19,7 +17,6 @@ import android.widget.Toast;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.FirebaseDatabase;
 
 
 /**
