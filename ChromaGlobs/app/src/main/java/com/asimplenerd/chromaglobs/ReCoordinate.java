32c94bebc43a3cbe7c5dcc5f91582c56d1dacc06
BYee7127 <byee7127@hotmail.com>
2020-04-01 22:37:44 -0600

    Got a line to draw... Finally

---
+++ ChromaGlobs/app/src/main/java/com/asimplenerd/chromaglobs/ReCoordinate.java
@@ -0,0 +1,19 @@
+package com.asimplenerd.chromaglobs;
+
+/**
+ * A holder object to contain the new coordinates for the new objects.
+ * May not be needed, but we'll see for now.
+ */
+public class ReCoordinate {
+    // coordinates on a screen
+    float x;
+    float y;
+
+    // secondary coordinates on the screen
+    float x2;
+    float y2;
+
+    // the width and height of an object, if applicable
+    float w;
+    float h;
+}
