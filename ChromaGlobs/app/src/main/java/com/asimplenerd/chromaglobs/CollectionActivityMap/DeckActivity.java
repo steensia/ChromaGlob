acce35d759cfe180da4214736067c7dfa2aadcce
Steen Sia <steensia101@gmail.com>
2020-04-06 15:52:55 -0600

    Added Deck Activity layout
    Might need to use ScrollView and SearchView elements in existing Collection/Deck layouts

---
+++ ChromaGlobs/app/src/main/java/com/asimplenerd/chromaglobs/CollectionActivityMap/DeckActivity.java
@@ -0,0 +1,16 @@
+package com.asimplenerd.chromaglobs.CollectionActivityMap;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.asimplenerd.chromaglobs.R;
+
+public class DeckActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_deck);
+    }
+}
